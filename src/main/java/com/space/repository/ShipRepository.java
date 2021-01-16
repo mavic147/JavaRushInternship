d6a9928f4a3a5d189be9e9d4233ac2a2b6ab7312
mavic147 <vamaximenko104@gmail.com>
2021-01-16 16:58:34 +0300

    unnecessary imports removed

--- src/main/java/com/space/repository/ShipRepository.java
+++ src/main/java/com/space/repository/ShipRepository.java
@@ -4,7 +4,5 @@ import com.space.model.Ship;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
-
 public interface ShipRepository extends JpaRepository<Ship, Long>, JpaSpecificationExecutor<Ship> {
 }
